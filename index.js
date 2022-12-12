8fdf432fdc884c1e8c7e7d169adcfec048bfde85
kevin <darkalp99@gmail.com>
2022-12-12 10:38:12 -0300

    update: logica

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+console.log(15 + 5);
+console.log(5 * 5);
+
+let nome = "Lucas Coelho";
+const dataNescimento = "31/10/1999";
+
+document.write("<h1>Hello World</h1>");
+document.write(` <h2>Nascido em: ${dataNescimento}</h2>`);
+
+document.getElementsByTagName("h1")[0].style.color = "blue";
+document.write(nome);
+document.write("<br>");
+nome = "Coelho";
+document.write(nome);
+document.write(dataNescimento);
